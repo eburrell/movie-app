428db725313c96ce6e9f0820343fc0be939682e5
michael <michaeleburrell@gmail.com>
2019-09-04 21:31:52 -0700

    adds validation, new columns and changes to controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -6,14 +6,19 @@ class Api::MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(
+      @movie = Movie.new(
       id: params[:id],
       title: params[:title],
       year: params[:year],
-      plot: params[:plot]
+      plot: params[:plot],
+      director: params[:director],
+      english: params[:english]
       )
-    @movie.save
-    render "show.json.jb"
+      if @movie.save
+        render "show.json.jb"
+      else
+        render json: {errors: @movie.errors.full_messages}, status: 422
+      end
   end
 
   def show
@@ -27,9 +32,14 @@ class Api::MoviesController < ApplicationController
     @movie.title = params[:title] || @movie.title
     @movie.year = params[:year] || @movie.year
     @movie.plot = params[:plot] || @movie.plot
+    @movie.director = params[:director] || @movie.director
+    @movie.english = params[:english] || @movie.english
 
-    @movie.save
-    render "show.json.jb" 
+    if @movie.save
+      render "show.json.jb"
+    else
+      render json: {errors: @movie.errors.full_messages}, status: 422
+    end  
   end
 
   def destroy
