befc00c1789e3f7f1a3101de73f6f9eabf8730c1
michael <michaeleburrell@gmail.com>
2019-09-12 15:31:20 -0700

    implements authorization

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,8 +1,9 @@
 class Api::MoviesController < ApplicationController
 
+  before_action :authenticate_admin, except: [:index, :show]
+
   def index
-    @movies = Movie.all
-    # @movies = Movie.where(english: :true)
+    @movies = Movie.all.where(english: :true)
     render "index.json.jb"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-
-# actor.save
-
-movie = Movie.new(title: "Training Day", year: 2001, plot: "Police drama about a veteran officer who escorts a rookie on his first day with the LAPD's tough inner-city narcotics unit.")
-
-movie.save
+Actor.create!([
+  {first_name: "George", last_name: "Clooney", known_for: "Ocean's (film series)", age: 30, gender: nil, movie_id: 4},
+  {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock", age: 25, gender: nil, movie_id: 1},
+  {first_name: "Robert", last_name: "Downey ", known_for: "Iron Man", age: 26, gender: nil, movie_id: 2},
+  {first_name: "Jack", last_name: "Nicholson", known_for: "Chinatown", age: 27, gender: nil, movie_id: 3}
+])
+Movie.create!([
+  {title: "Up", year: 2009, plot: "Carl Fredricksen, a 78-year-old balloon salesman, is about to fulfill a lifelong dream.", director: nil, english: nil},
+  {title: "Lion King", year: 1994, plot: "A Lion cub crown prince is tricked by a treacherous uncle into thinking he caused his father's death and flees into exile in despair, only to learn in adulthood his identity and his responsibilities.", director: nil, english: nil},
+  {title: "Murder on the Orient Express", year: 2017, plot: "A lavish trip through Europe quickly unfolds into a race against time to solve a murder aboard a train. ", director: nil, english: nil},
+  {title: "Zootopia", year: 2016, plot: "Determined to prove herself, Judy jumps at the opportunity to solve a mysterious case. ", director: nil, english: nil},
+  {title: "Hola\n", year: 2017, plot: "Saying hi.", director: nil, english: nil},
+  {title: "Training Day", year: 2001, plot: "Police drama about a veteran officer who escorts a rookie on his first day with the LAPD's tough inner-city narcotics unit.", director: nil, english: true}
+])
