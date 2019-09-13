5179085766eca7a9eeeb47f8844c746255c4c9a4
michael <michaeleburrell@gmail.com>
2019-09-13 14:58:25 -0700

    adds more models and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   # validates_format_of :title, with: /[a-zA-Z0-9]/
   # validates :title, presence: true
 end
